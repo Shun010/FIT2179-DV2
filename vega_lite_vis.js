38a7076251a3158b36ff609830930c86bb35660a
Shun010 <skaw0010@student.monash.edu>
2026-05-19 03:10:01 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,53 @@
+//Graph 1: Gold Production of Each State from 1851 to 2021
+var vg_1 = "gold_production_by_state.vg.json";
+vegaEmbed("#gold_production_state", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+//Graph 2: Population in Australia, from 1800 to 1890
+var vg_2 = "aus_population_history.vg.json";
+vegaEmbed("#aus_population_history", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+//Graph 3: Consumer Price of Index from 1850 to 1890
+var vg_3 = "aus_CPI_history.vg.json";
+vegaEmbed("#aus_CPI_history", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+//Graph 4: Ore Grade
+var vg_4 = "aus_ore_grade_history.vg.json";
+vegaEmbed("#aus_ore_grade_history", vg_4).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+//Graph 5: Operating mines in 2015
+var vg_5 = "aus_operating_mines.vg.json";
+vegaEmbed("#aus_operating_mines", vg_5).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+//Graph 6: Top 10 Gold-Producing Countries in 2024
+var vg_6 = "world_gold_rank.vg.json";
+vegaEmbed("#world_gold_rank", vg_6).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+//Graph 7: Gold Price vs Gold Production in Australia
+var vg_7 = "gold_price_production.vg.json";
+vegaEmbed("#gold_price_production", vg_7).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+//Graph 8: Gold Exploration Expenditure
+var vg_8 = "gold_exploration_expenditure.vg.json";
+vegaEmbed("#gold_exploration_expenditure", vg_8).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+//Graph 9: Gold Demand
+var vg_9 = "gold_demand.vg.json";
+vegaEmbed("#gold_demand", vg_9).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
